b535f6d98c9c8014d83f24e6e6ae61f8680c13f8
Mykola Odnosumov <mkloouo@gmail.com>
2020-03-04 08:23:11 +0200

    Fix imports and image scaling

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,33 @@
 import React from 'react';
-import {View, Text, Image, StyleSheet} from 'react-native';
+import {Text, Image, StyleSheet, SafeAreaView} from 'react-native';
 
 class App extends React.Component {
   render() {
+    const reactNativeJpg = require('../images/react-native.jpg');
+
     return (
-      <View style={s.container}>
+      <SafeAreaView style={s.container}>
         <Text style={s.text}>Hello, React!</Text>
-        <Image
-          style={s.image}
-          source={{uri: 'https://i.imgur.com/eoTELkV.jpg'}}
-        />
-      </View>
+        <Image style={s.image} source={reactNativeJpg} />
+      </SafeAreaView>
     );
   }
 }
 
 const s = StyleSheet.create({
   container: {
+    marginTop: 100,
+    marginBottom: 100,
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   text: {
     fontSize: 32,
   },
   image: {
-    width: 500,
-    height: 'auto',
+    height: 400,
+    width: 350,
   },
 });
 
